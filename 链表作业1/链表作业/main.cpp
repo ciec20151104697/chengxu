31941dc1f869a87c1ff4c7f1dc4a6bef6c96dd98
钟洋洋 <1768696618@qq.com>
2016-12-19 00:03:15 +0800

    Add files via upload

---
+++ 链表作业1/链表作业/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  链表作业
+//
+//  Created by 20151104697 on 16/11/28.
+//  Copyright © 2016年 20151104697. All rights reserved.
+//
+
+#include<stdio.h>
+#define N 5
+struct Node
+{
+    char name[40];
+    int age;
+    struct Node *next;
+};
+int main()
+{
+    struct Node * head,*p,*tail;
+    int i;
+    head=new Node;
+    head->next=0;
+    tail=head;
+    scanf("%s",head->name);
+    scanf("%d",&head->age);
+    for (i=0;i<4;i++)
+    {
+        p=new Node;
+        scanf("%s",p->name);
+        scanf("%d",&p->age);
+        p->next=0;
+        tail->next=p;
+        tail=p;
+    }
+    p=head;
+    while(p)
+    {
+        printf("%s %d\n",p->name,p->age);
+        p=p->next;
+    }
+    return 0;
+}
